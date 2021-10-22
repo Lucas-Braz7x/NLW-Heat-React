b8c7cb5fe224588b88aff1a2f14ff124db651059
Lucas-Braz7x <lucasdutrabraz@gmail.com>
2021-10-22 19:02:52 -0300

    Add AuthContext

---
+++ src/App.tsx
@@ -0,0 +1,20 @@
+import React, { useContext } from 'react'
+import styles from './App.module.scss'
+import { LoginBox } from './components/LoginBox'
+import { MessageList } from './components/MessageList'
+import { SendMessageForm } from './components/SendMessageForm'
+import { AuthContext } from './contexts/auth'
+
+export function App() {
+  const { user } = useContext(AuthContext);
+
+  // !! -> Transforma a variável em um boolean 
+  /* if(!!user){} */
+
+  return (
+    <main className={`${styles.contentWrapper} ${!!user ? styles.contentSigned : ''}`}>
+      <MessageList />
+      {!!user ? <SendMessageForm /> : <LoginBox />}
+    </main>
+  )
+}
